30f94e44266b7c5f2dbeea9b695f92f9c45fff1b
Harris Bisset <RedstoneRebel399@gmail.com>
2024-01-19 16:59:52 +0000

    dark mode fix

--- view/layout/layout_templ.go
+++ view/layout/layout_templ.go
@@ -23,7 +23,7 @@ func Base() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<!doctype html><html class=\"dark\"><head><title>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<!doctype html><html><head><title>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
