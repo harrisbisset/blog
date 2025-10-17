58e6795c3587a6033e73a80528d221c38fa2605a
harrisbisset <RedstoneRebel399@gmail.com>
2025-10-17 22:16:57 +0100

    fix search blur

--- src/lib/components/util/boxcase.svelte
+++ src/lib/components/util/boxcase.svelte
@@ -18,23 +18,21 @@
 <section style={`width: ${width};`} class="min-w-(--standard-width)">
 	<div class="flex flex-col gap-1">
 		<div>
-			<div class="relative z-10 text-2xl font-bold lowercase">
-				<div class="relative z-10 text-2xl font-bold lowercase">
-					{#if typeof TitleComponent === 'string'}
-						{TitleComponent}
-					{:else}
-						<TitleComponent />
-					{/if}
-				</div>
-				<div
-					class={[
-						colour === 'orange' && 'border-highlight-1 shadow-highlight-1',
-						colour === 'blue' && 'border-background-2 shadow-background-2',
-						'relative -top-2.5 z-0 w-full border-t-10  shadow-[0_4px_10px_-2px] '
-					]}
-				></div>
+			<div class="text-2xl font-bold lowercase">
+				{#if typeof TitleComponent === 'string'}
+					{TitleComponent}
+				{:else}
+					{@render TitleComponent()}
+				{/if}
 			</div>
-			{@render children?.()}
+			<div
+				class={[
+					colour === 'orange' && 'border-highlight-1 shadow-highlight-1',
+					colour === 'blue' && 'border-background-2 shadow-background-2',
+					'relative -z-10 -top-2.5 w-full border-t-10  shadow-[0_4px_10px_-2px] '
+				]}
+			></div>
 		</div>
+		{@render children?.()}
 	</div>
 </section>
